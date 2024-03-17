23ff8c60506fd006d2dc3c1c6b07fbcc598b995f
PR <>
2024-03-16 18:41:01 -0700

    put everything online just to text animations

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.menu-button').forEach(button => {
+      button.addEventListener('click', function() {
+          const currentMenu = this.nextElementSibling; // Should correctly target the next sibling which is the `.secondary-menu`
+          
+          // Check if the current menu is already expanded
+          const isExpanded = currentMenu.classList.contains('expanded');
+
+          // First, close all menus
+          document.querySelectorAll('.secondary-menu').forEach(menu => {
+              menu.classList.remove('expanded');
+          });
+
+          // Then, if the current menu was not already expanded, expand it
+          if (!isExpanded) {
+              currentMenu.classList.add('expanded');
+          }
+      });
+  });
+});
